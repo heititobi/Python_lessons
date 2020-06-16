23366336ce8d912ffad6351c548a84ca0063dd59
Heiti <heiti.tobi@gmail.com>
2020-06-16 14:39:47 +0300

    Code review fixes

--- Estonian_ID_generator.py
+++ Estonian_ID_generator.py
@@ -1,42 +1,47 @@
+# General ruling for Estonian ID - https://et.wikipedia.org/wiki/Isikukood
 from random import randint
 
-#General ruling for Estonian ID - https://et.wikipedia.org/wiki/Isikukood
-# 1. = sugu ja sünniaasta esimesed kaks numbrit (1...6)
-# 2. ja 3. = sünniaasta 3. ja 4. numbrid (00...99)
-# 4. ja 5. = sünnikuu (01...12)
-# 6. ja 7. = sünnikuupäev (01...31)
-# 8., 9. ja 10. number = järjekorranumber samal päeval sündinute eristamiseks (000...999). Enne 2013. aastat sündinute puhul võib sisaldada haigla tunnust.
-# 11. = kontrollnumber (0...9)
 
 # Generates random string of number taking the range and the needed number of digits as an input
 def number(start, stop, digits=1):
     random_number = str(randint(start, stop))
     return random_number.zfill(digits)
 
+
+# Mathematical calculation of Estonian ID number and its modulo 11 for the last digit of the ID
+def modulo_11(code, step=1):
+    first_step = [1, 2, 3, 4, 5, 6, 7, 8, 9, 1]
+    second_step = [3, 4, 5, 6, 7, 8, 9, 1, 2, 3]
+    modulo = 0
+    if step == 1:
+        for index, element in enumerate(str(code)):
+            modulo += int(element) * first_step[index]
+    else:
+        print("Step2")
+        for index, element in enumerate(str(code)):
+            modulo += int(element) * second_step[index]
+    return str(modulo % 11)
+
+
 # Calculates Estonian ID-s last digit checksum based on first 10 digits
-def checksum_calculation(id):
-    first_step = [1,2,3,4,5,6,7,8,9,1]
-    second_step = [3,4,5,6,7,8,9,1,2,3]
-    checksum_first = 0
-    checksum_second = 0
-    for index, number in enumerate(id):
-        checksum_first += int(number)*first_step[index]
-    if checksum_first % 11 == 10:
-        for index, number in enumerate(id):
-            checksum_second += int(number)*second_step[index]
-        return checksum_second if checksum_first % 11 != 10 else 0
-    return checksum_first % 11
+def checksum_calculation(id_number):
+    checksum = modulo_11(id_number)
+    if int(checksum) != 10:
+        return checksum
+    else:
+        return modulo_11(id_number, step=2)
+
 
 # Generates random Estonian ID code GYYMMDDSSSC and returns it as a string. G shows sex and century of birth,
 # SSS is a serial number separating persons born on the same date and C a checksum
-def Estonian_ID():
-    gender = number(1,6)
-    year = number(0,99,2)
-    month = number(1,12,2)
-    day = number(1,28,2)
-    serial_number = number(0,999,3)
-    ID = gender + year + month + day + serial_number
-    return f'{ID}{checksum_calculation(ID)}'
+def estonian_id():
+    gender = number(1, 6)
+    year = number(0, 99, 2)
+    month = number(1, 12, 2)
+    day = number(1, 28, 2)
+    serial_number = number(0, 999, 3)
+    first_ten_id_numbers = f'{gender}{year}{month}{day}{serial_number}'
+    return f'{first_ten_id_numbers}{checksum_calculation(first_ten_id_numbers)}'
 
-print(Estonian_ID())
 
+print(estonian_id())
